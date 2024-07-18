de8718dec2554a01449f9fe3254ac209a6e93c64
Skyline Buildestates <skylinebuildestatespvt@gmail.com>
2024-07-18 17:59:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Header />
       <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/career" component={<Career />} />
+          <Route path="/career" element={<Career />} />
       </Routes>
 
       <Footer/>
